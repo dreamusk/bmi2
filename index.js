45546e9488e620a38bd41e712cda61e0b7dacd3f
ANKIT TRIPATHI <98980994+dreamusk@users.noreply.github.com>
2022-06-27 22:30:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function bmiCalculator (){
+    var h = prompt("height");
+    var w = prompt("weight");
+   var bmi = h/(w*w);
+   alert(bmi);
+   document.getElementById("pil").innerText=bmi;
+};
+
+bmiCalculator();
+
